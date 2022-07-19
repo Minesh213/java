5ceaa182b022aeb7a4be9dab176a4db60802d02e
Minesh Valand <mpvaland21395@gmail.com>
2022-07-19 21:07:34 +0100

    updated file

--- tablework1.java
+++ tablework1.java
@@ -1,21 +1,21 @@
 public class tablework1 {
     public static void main(String[] args) {
-        int a=10;
-        int b=5;
-        System.out.println(a+b);//15
+        int a=15;
+        int b=10;
+        System.out.println(a+b);//25
         System.out.println(a-b);//5
-        System.out.println(a*b);//50
-        System.out.println(a/b);//2
-        System.out.println(a++);//10
-        System.out.println(++a);//12
-        System.out.println(b++);//5
-        System.out.println(++b);//7
-        System.out.println(a--);//12
-        System.out.println(--a);//10
-        System.out.println(b--);//7
-        System.out.println(--b);//5
-        System.out.println(a);//10
-        System.out.println(b);//5
+        System.out.println(a*b);//150
+        System.out.println(a/b);//1
+        System.out.println(a++);//15
+        System.out.println(++a);//17
+        System.out.println(b++);//10
+        System.out.println(++b);//12
+        System.out.println(a--);//17
+        System.out.println(--a);//15
+        System.out.println(b--);//12
+        System.out.println(--b);//10
+        System.out.println(a);//15
+        System.out.println(b);//10
 
     }
 }
